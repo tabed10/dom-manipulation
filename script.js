7d53b94fbf15654fb4ab86a10a5fc6377ae53b6f
tabed10 <sabbirhossainabed@gmail.com>
2019-06-12 00:56:40 +0600

    task 2a (incomplete)

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ Element = document.title
 console.log(Element)
 
 /* Task 2 using plain JavaScript */
-
-
+function addTxt(){
+    var el1 = document.getElementById("task2a")
+    el1.innerHTML = "Hello World"
+}
+var el2 = document.getElementById("btn") 
+el2.addEventListener("click",addTxt)
 
 /* Task 4 using jQuery */
