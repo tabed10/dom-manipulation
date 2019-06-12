b23e58ee7bb50e54000cfeeddb8a2e5b2649ce99
cis101 <cis101@secs.iub.edu.bd>
2019-06-12 09:11:25 +0600

    task 4c

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ var el2 = document.getElementById("btn")
 el2.addEventListener("click",addTxt)
 
 /* Task 4 using jQuery */
+//Task 4a
+$('#addText2').click(function(event){
+    var paragraph = $('<p>').text('Hello World')
+    $('#task4a').append(paragraph)
+})
+//Task 4b
+function changeBG(event){
+    var color = event.target.innerText.toLowerCase()
+    $('body').css('background-color',color)
+}
+//Task 4c
+
+$('#add').click(function(event){
+    var sum = Number($('#num1').val()) + Number($('#num2').val())
+    $('#sum').text(sum)
+})
